09bc514164213952debef3350a84328017f32c33
nick <7zx.1zx@gmail.com>
2023-02-28 13:27:51 +0300

    gunicorn

--- main.py
+++ main.py
@@ -65,6 +65,5 @@ def not_found(e):
 def server_error(e):
     return 'ошибка 500, Internal Server Error'
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
